ac34bd29d1c37ea4393c4b64ea4ac6a100200de5
Nifer Kilakila <nifercritter@gmail.com>
2023-02-02 21:19:19 -0800

    card list props

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,16 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 class CardList extends Component {
-
-    render () {
-        return (
-        <div>Carlist Component</div>
-        )
-    }
+  render() {
+    const { monsters } = this.props;
+    return (
+      <div>
+        {monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default CardList;
